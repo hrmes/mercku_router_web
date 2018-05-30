b59f1251df3b23d1069f7a00864e7c1ed3122448
patrick <patrick.fan.sc@gmail.com>
2018-05-30 23:30:13 +0800

    update version.js

--- build/version.js
+++ build/version.js
@@ -2,25 +2,19 @@ const fs = require('fs');
 const path = require('path');
 var exec = require('child_process').exec;
 
-console.log(exec)
-
 const fsPath = path.join(__dirname, '../version.json');
 fs.readFile(fsPath, 'utf8', (err, data) => {
   if (err) throw err;
   data = JSON.parse(data);
-  console.log(`current Web version is ${data.version}`);
   data.version += 1;
-  console.log(`next Web version is ${data.version}`);
+  console.log(`new web version is ${data.version}`);
   fs.writeFile(fsPath, JSON.stringify(data), err => {
     if (err) throw err;
-    console.log('write new version ok.');
+    console.log('write version ok.');
     console.log('git add version.json');
     exec('git add version.json', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
+      if (err) throw err;
+      console.log('git add version.json ok.')
     });
   });
 });
