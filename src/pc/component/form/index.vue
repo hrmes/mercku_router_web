846f7ce269aaee1781ec441686a4a88d648e4b59
patrick <patrick.fan.sc@gmail.com>
2018-06-03 12:01:19 +0800

    update form validate

--- src/pc/component/form/index.vue
+++ src/pc/component/form/index.vue
@@ -7,27 +7,14 @@
 export default {
   props: ['model', 'rules'],
   methods: {
-    check(value, validators) {
-      if (validators && validators.length) {
-        for (let i = 0; i < validators.length; i += 1) {
-          const validator = validators[i];
-          if (!validator.rule(value)) {
-            return false;
-          }
-        }
-      }
-      return true;
-    },
     validate() {
-      const keys = Object.keys(this.model);
-      for (let i = 0; i < keys.length; i += 1) {
-        const value = this.model[keys[i]];
-        const validator = this.rules[keys[i]];
-        if (!this.check(value, validator)) {
-          return false;
+      let result = true;
+      this.$children.forEach(child => {
+        if (!child.validate()) {
+          result = false;
         }
-      }
-      return true;
+      });
+      return result;
     }
   }
 };
