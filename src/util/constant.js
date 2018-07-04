5423e529c5d062916182d9e564990d412ac12ea4
两瓶阿萨姆丶 <vivibearsir@163.com>
2018-07-04 14:21:48 +0800

    const

--- src/util/constant.js
+++ src/util/constant.js
@@ -3,5 +3,21 @@ export const WanType = {
   static: 'static',
   dhcp: 'dhcp'
 };
+export const wanNetStatus = {
+  testing: 'testing',
+  connected: 'connected',
+  linked: 'linked',
+  unlinked: 'unlinked'
+}
+export const routerSnModel = {
+  '00': '电源适配器',
+  '01': 'M2',
+  '02': 'Bee'
+}
+export const speedTestStatus = {
+  testing: 'testing',
+  done: 'done',
+  failed: 'failed'
+}
 
 export default WanType;
--- src/util/util.js
+++ src/util/util.js
@@ -1,5 +1,5 @@
 export default {
-  adapt: () => {
+  adapt : () => {
     const doc = document;
     const win = window;
     const docEl = doc.documentElement;
@@ -21,18 +21,20 @@ export default {
         needWidth = width;
       }
       rem = needWidth / 3.75;
-      rem = rem > 100 ? 100 : rem;
+      rem = rem > 100
+        ? 100
+        : rem;
       // const rem = width / 3.75;
       rootStyle = `html{font-size:${rem}px !important;width:${needWidth}px !important;height:${height}px !important}`;
-      rootItem =
-        document.getElementById('rootsize') || document.createElement('style');
+      rootItem = document.getElementById('rootsize') || document.createElement('style');
       if (!document.getElementById('rootsize')) {
-        document.getElementsByTagName('head')[0].appendChild(rootItem);
+        document
+          .getElementsByTagName('head')[0]
+          .appendChild(rootItem);
         rootItem.id = 'rootsize';
       }
       if (rootItem.styleSheet) {
-        rootItem.styleSheet.disabled ||
-          (rootItem.styleSheet.cssText = rootStyle);
+        rootItem.styleSheet.disabled || (rootItem.styleSheet.cssText = rootStyle);
       } else {
         try {
           rootItem.innerHTML = rootStyle;
@@ -47,28 +49,20 @@ export default {
     // win.addEventListener(   'resize',   () => {     clearTimeout(tid);     tid =
     // setTimeout(refreshRem, 300);   },   false );
 
-    win.addEventListener(
-      'pageshow',
-      e => {
-        if (e.persisted) {
-          // 浏览器后退的时候重新计算
-          clearTimeout(tid);
-          tid = setTimeout(refreshRem, 300);
-        }
-      },
-      false
-    );
+    win.addEventListener('pageshow', e => {
+      if (e.persisted) {
+        // 浏览器后退的时候重新计算
+        clearTimeout(tid);
+        tid = setTimeout(refreshRem, 300);
+      }
+    }, false);
 
     if (doc.readyState === 'complete') {
       doc.body.style.fontSize = '16px';
     } else {
-      doc.addEventListener(
-        'DOMContentLoaded',
-        () => {
-          doc.body.style.fontSize = '16px';
-        },
-        false
-      );
+      doc.addEventListener('DOMContentLoaded', () => {
+        doc.body.style.fontSize = '16px';
+      }, false);
     }
   }
 };
@@ -81,7 +75,7 @@ export const isIphone = () => {
 };
 
 export const ipRexp = ip => {
-  const pattern = /^(?:(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])\.){3}(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])$/;
+  const pattern = /^(?:(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])\.){3}(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])$/qwq eeeqw;
   if (ip && pattern.test(ip)) {
     return true;
   }
