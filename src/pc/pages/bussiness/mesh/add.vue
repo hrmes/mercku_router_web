7557f0823d98f56e41dc63722eabbecac6b18fc5
Patrick <patrick.fan.sc@gmail.com>
2018-09-18 15:29:22 +0800

    update

--- src/pc/pages/bussiness/mesh/add.vue
+++ src/pc/pages/bussiness/mesh/add.vue
@@ -1,16 +1,66 @@
 <template>
   <layout>
-    <div>添加mesh节点</div>
+    <div class="mesh-container">
+      <div class="content">
+        <div class='w-header'>
+          {{$t('trans0194')}}
+        </div>
+      </div>
+    </div>
   </layout>
 </template>
 <script>
+import Form from '../../../component/form/index.vue';
+import FormItem from '../../../component/formItem/index.vue';
+import Input from '../../../component/input/input.vue';
+import Progress from '../../../component/progress/index.vue';
 import layout from '../../../layout.vue';
+import { passwordRule } from '../../../../util/util';
 
 export default {
   components: {
     layout
-  }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
 };
 </script>
-<style>
+<style lang="scss" scoped>
+.mesh-container {
+  position: relative;
+  flex: auto;
+  padding: 0 2%;
+  display: flex;
+  .content {
+    border-radius: 8px;
+    padding: 0 20px;
+    background: white;
+    position: relative;
+    flex: 1;
+    .w-header {
+      height: 60px;
+      border-bottom: 1px solid #f1f1f1;
+      font-size: 16px;
+      color: #333333;
+      line-height: 60px;
+      font-weight: 400;
+    }
+  }
+}
+@media screen and (max-width: 768px) {
+  .mesh-container {
+    padding: 20px 16px;
+    .content {
+      .w-header {
+        font-size: 14px;
+        height: 44px;
+        line-height: 44px;
+      }
+      min-height: 450px;
+    }
+  }
+}
 </style>
+
