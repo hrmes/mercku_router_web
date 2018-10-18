90de7b7f2c06d9468da8f16c1bb833079d9cb144
Patrick <patrick.fan.sc@gmail.com>
2018-10-18 17:22:12 +0800

    update bandwidth

--- src/util/util.js
+++ src/util/util.js
@@ -244,17 +244,17 @@ export const formatSpeed = value => {
   return { value: '-', unit: 'KB' };
 };
 export const formatBandWidth = value => {
-  const units = ['B', 'K', 'M', 'G', 'T', 'P'];
+  const units = ['bps', 'K', 'M', 'G', 'T', 'P'];
   let index = 0;
   if (!isNaN(value)) {
-    do {
+    while (value > 1024 && index < units.length - 1) {
       value /= 1024;
       index += 1;
-    } while (value > 1024 && index < units.length - 1);
+    }
     return {
       value: value.toFixed(1),
       unit: units[index]
     };
   }
-  return { value: '-', unit: 'B' };
+  return { value: '-', unit: 'b' };
 };
