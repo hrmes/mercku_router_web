9aa0a2c351e707592f88cfee3bf8390d1042821b
两瓶阿萨姆丶 <vivibearsir@163.com>
2018-10-19 14:23:17 +0800

    util

--- src/pc/pages/bussiness/dashboard/device.vue
+++ src/pc/pages/bussiness/dashboard/device.vue
@@ -66,8 +66,8 @@
             </li>
             <li class="column-band" v-if='isMobile?(row.expand):true'>
               <span>{{$t('trans0015')}}</span>
-              <span> {{formatBandWidth(row.online_info.traffic.ul+row.online_info.traffic.dl).value}}</span>
-              <span> {{formatBandWidth (row.online_info.traffic.ul+row.online_info.traffic.dl).unit}}B</span>
+              <span> {{formatNetworkData(row.online_info.traffic.ul+row.online_info.traffic.dl).value}}</span>
+              <span> {{formatNetworkData (row.online_info.traffic.ul+row.online_info.traffic.dl).unit}}</span>
             </li>
             <li class="column-ip device-item" v-if='isMobile?(row.expand):true'>
               <span>{{$t('trans0151')}}</span>
