4590178891df3635146ba86a5165466c9f7f26d7
Patrick <patrick.fan.sc@gmail.com>
2018-11-17 13:24:53 +0800

    update

--- src/pc/pages/bussiness/advance/vpn/form.vue
+++ src/pc/pages/bussiness/advance/vpn/form.vue
@@ -7,16 +7,16 @@
       <div class="form">
         <m-form ref="form" :model="form" :rules='rules'>
           <m-form-item class="item" prop='name'>
-            <m-input :label="$t('trans0108')" type="text" :placeholder="$t('trans0440')" v-model="form.name" />
+            <m-input :label="$t('trans0108')" type="text" :placeholder="$t('trans0321')" v-model="form.name" />
           </m-form-item>
           <m-form-item class="item" prop='protocol'>
             <m-select :label="$t('trans0408')" v-model="form.protocol" :options="protocols"></m-select>
           </m-form-item>
           <m-form-item class="item" prop='server'>
-            <m-input :label="$t('trans0409')" type="text" :placeholder="$t('trans0440')" v-model="form.server" />
+            <m-input :label="$t('trans0409')" type="text" :placeholder="$t('trans0321')" v-model="form.server" />
           </m-form-item>
           <m-form-item class="item" prop='username'>
-            <m-input :label="$t('trans0410')" type="text" :placeholder="$t('trans0440')" v-model="form.username" />
+            <m-input :label="$t('trans0410')" type="text" :placeholder="$t('trans0321')" v-model="form.username" />
           </m-form-item>
           <m-form-item class="item" prop='password'>
             <m-input :label="`${$t('trans0003')}${$t('trans0411')}`" type='password' :placeholder="`${$t('trans0321')}`" v-model="form.password"></m-input>
