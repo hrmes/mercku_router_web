2e58f9e1e9eaa79d84fb97f4af5d6f45e4e79ae8
Patrick <patrick.fan.sc@gmail.com>
2018-11-21 15:25:50 +0800

    update

--- src/pc/pages/bussiness/advance/tr069.vue
+++ src/pc/pages/bussiness/advance/tr069.vue
@@ -7,6 +7,9 @@
       <div class="page-content">
         <m-form ref="remote" class="form" :model="remote" :rules="remoateRules">
           <div class="title">{{$t('trans0491')}}</div>
+          <m-form-item>
+            <m-input :label="`${$t('trans0498')}`" v-model="remote.url"></m-input>
+          </m-form-item>
           <m-form-item prop="username">
             <m-input :label="$t('trans0410')" v-model="remote.username"></m-input>
           </m-form-item>
@@ -16,9 +19,7 @@
           <m-form-item prop="interval">
             <m-input :label="$t('trans0490')" v-model="remote.interval"></m-input>
           </m-form-item>
-          <m-form-item>
-            <m-input :label="`${$t('trans0498')}${$t('trans0411')}`" v-model="remote.url"></m-input>
-          </m-form-item>
+
         </m-form>
         <m-form ref="local" class="form" :model="local" :rules="localRules">
           <div class="title">{{$t('trans0493')}}</div>
@@ -26,13 +27,13 @@
             <m-input :label="$t('trans0494')" v-model="local.path"></m-input>
           </m-form-item>
           <m-form-item prop="path">
-            <m-input :label="$t('trans0495')" v-model="remote.port"></m-input>
+            <m-input :label="$t('trans0495')" v-model="local.port"></m-input>
           </m-form-item>
           <m-form-item>
-            <m-input :label="`${$t('trans0410')}${$t('trans0411')}`" v-model="remote.username"></m-input>
+            <m-input :label="`${$t('trans0410')}${$t('trans0411')}`" v-model="local.username"></m-input>
           </m-form-item>
           <m-form-item>
-            <m-input :label="`${$t('trans0003')}${$t('trans0411')}`" v-model="remote.password"></m-input>
+            <m-input :label="`${$t('trans0003')}${$t('trans0411')}`" v-model="local.password"></m-input>
           </m-form-item>
           <div class="form-item">
             <m-checkbox :text="$t('trans0492')" v-model="enabled"></m-checkbox>
