097736ce33bfd340f47964e09499a7e9e3a6cc8c
Patrick <patrick.fan.sc@gmail.com>
2018-11-22 18:00:43 +0800

    update

--- i18n.sh
+++ i18n.sh
@@ -28,7 +28,8 @@ fi
 # 切换到指定分支
 echo -e "\033[32mcheckout $doc_branch...\033[0m"
 git checkout $doc_branch
-git pull
+git pull origin $doc_branch
+
 # 进入翻译文件目录
 echo -e "\033[32mcd mercku/trans/app_code...\033[0m"
 cd mercku/trans/app_code
@@ -36,5 +37,16 @@ cd mercku/trans/app_code
 echo -e "\033[32mcopy files...\033[0m"
 cp -f $sourceENFile $project_dir/src/i18n/$targetENFile
 cp -f $sourceZHFile $project_dir/src/i18n/$targetZHFile
+cp -f $sourceErrorFile $project_dir/src/i18n/$targetErrorFile
+echo -e "\033[32mcomplete...\033[0m"
+
+# 进入CIK翻译文件目录
+cd ..
+echo -e "\033[32mcd mercku/trans/CIK_code...\033[0m"
+cd CIK_code
+# 拷贝文件
+echo -e "\033[32mcopy files...\033[0m"
+cp -f $sourceENFile $project_dir/src/i18n/cik-$targetENFile
+cp -f $sourceZHFile $project_dir/src/i18n/cik-$targetZHFile
 cp -f $sourceErrorFile $project_dir/src/i18n/$targetErrorFile
 echo -e "\033[32mcomplete...\033[0m"
