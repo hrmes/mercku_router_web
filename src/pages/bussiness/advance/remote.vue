210c23fe2e29a6587abffcc3a9a3747632fd055c
Patrick <penghui.fan@hyku.com>
2019-01-08 15:28:36 +0800

    update

--- src/pages/bussiness/advance/remote.vue
+++ src/pages/bussiness/advance/remote.vue
@@ -243,20 +243,20 @@ export default {
     };
   },
   computed: {
-    ...(() => {
-      const Tabs = {
-        isTR069: 'tr069',
-        isTFTP: 'tftp',
-        isTelnet: 'telnet',
-        isWWA: 'wwa'
-      };
-      const result = {};
-      const { category } = this.$route.params;
-      Object.keys(Tabs).forEach(key => {
-        result[key] = () => category === Tabs[key];
-      });
-      return result;
-    })()
+    // ...(() => {
+    //   const Tabs = {
+    //     isTR069: 'tr069',
+    //     isTFTP: 'tftp',
+    //     isTelnet: 'telnet',
+    //     isWWA: 'wwa'
+    //   };
+    //   const result = {};
+    //   const { category } = this.$route.params;
+    //   Object.keys(Tabs).forEach(key => {
+    //     result[key] = () => category === Tabs[key];
+    //   });
+    //   return result;
+    // })()
   },
   mounted() {
     this.$http.getTelnetEnabled().then(res => {
