123081ef16acb11930e23bff93c7bdbfc0f3000e
Patrick <penghui.fan@hyku.com>
2019-01-15 17:54:48 +0800

    remove comments

--- src/util/upgrade.js
+++ src/util/upgrade.js
@@ -15,31 +15,5 @@ export default function upgradeHelper(indexHtml) {
     document.write(indexHtml);
   } else {
     console.log('the x-web-version-hash is same, i think router not upgrade');
-    // document.body.querySelector('#app-wrap').remove();
-    // for test in here
-
-    // const scripts = document.querySelectorAll('script');
-    // scripts.forEach(s => s.remove());
-    // // const links = document.querySelectorAll('link');
-    // // links.forEach(l => l.remove());
-
-    // const appWrap = document.body.querySelector('#app-wrap');
-    // Array.from(appWrap.childNodes).forEach(node => {
-    //   node.remove();
-    // });
-
-    // const el = document.createElement('div');
-    // el.id = 'web';
-    // appWrap.append(el);
-
-    // meta.setAttribute('content', newHash);
-
-    // // const script = document.createElement('script');
-    // // script.src = '/index.js';
-    // // document.body.append(script);
-    // const newScripts = /<script src="([/\w.]*)".*><\/scirpt>/g.exec(indexHtml);
-    // const newlinks = /<link href="([/\w.]*)">/g.exec(indexHtml);
-
-    // console.log(newScripts, newlinks);
   }
 }
