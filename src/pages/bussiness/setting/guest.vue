61337c8ddc0e8b4f25ca622b4a16756749ef7eb2
两瓶阿萨姆丶 <vivibearsir@163.com>
2019-01-16 10:58:52 +0800

    guest

--- src/pages/bussiness/setting/guest.vue
+++ src/pages/bussiness/setting/guest.vue
@@ -70,7 +70,7 @@
                     @click='cancel'>{{$t('trans0025')}}</button>
           </div>
         </div>
-        <div v-if="showStatusPage">
+        <div v-if="form.enabled&&showStatusPage">
           <div class="setting-ssid-info">
             <div class="title">
               {{$t('trans0168')}}
@@ -231,8 +231,8 @@ export default {
     },
     guestEnabledChange(enabled) {
       if (
-        (!enabled && this.setupAndStart && !this.showSettingPage)
-        || (this.showSettingPage && this.showCancelBtn)
+        (!enabled && this.setupAndStart && !this.showSettingPage) ||
+        (this.showSettingPage && this.showCancelBtn)
       ) {
         this.$dialog.confirm({
           okText: this.$t('trans0024'),
