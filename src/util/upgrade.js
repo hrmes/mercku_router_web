1859c9e9494f82aa591947c076dcfc90b7deb0c0
PengHui Fan <penghui.fan@hyku.com>
2019-06-28 18:56:50 +0800

    Add console

--- src/util/upgrade.js
+++ src/util/upgrade.js
@@ -11,10 +11,12 @@ export default function upgradeHelper(indexHtml) {
   }
   console.log(`old hash is: ${oldHash}`);
   console.log(`new hash is: ${newHash}`);
+  console.log(`html is: ${indexHtml}`);
   if (!oldHash || newHash !== oldHash) {
     console.log('the x-web-version-hash is not same, i think router has upgrade ');
-    document.clear();
+    console.log(`document is: ${document.body}`);
     document.write(indexHtml);
+    console.log(`after write document is: ${document.body}`);
   } else {
     console.log('the x-web-version-hash is same, i think router not upgrade');
   }
