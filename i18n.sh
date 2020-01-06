04adaac6272ef97dfe631dffb538e5d2271bbf54
PengHui Fan <penghui.fan@hyku.com>
2020-01-06 14:39:40 +0800

    Update config

--- i18n.sh
+++ i18n.sh
@@ -12,52 +12,68 @@ target=(en-US.json zh-CN.json de-DE.json nl-NL.json)
 sourceerror=error_to_trans_web.json
 targeterror=code-map.json
 
+# $1 is message for echo
+# $2 is echo type, support error|success
+beautify_echo(){
+  if [ $2 == error ]
+  then
+    echo -e "\033[31merror: $1\033[0m"
+  elif [ $2 == success ]
+  then
+    echo -e "\033[32msuccess: $1\033[0m"
+  elif [ $2 == info ]
+  then
+    echo -e "\033[37minfo: $1\033[0m"
+  fi
+}
+
 if [ ${#source[*]} != ${#target[*]} ];then
-  echo -e "\033[31marray length not match...\033[0m"
+  beautify_echo "array length not match..." error
   exit -1
 fi
 
 # 检查是否指定分支
 if [ "$doc_branch" == "" ];then
-  echo -e "\033[31mmiss branch...\033[0m"
+  beautify_echo "miss branch..." error
   exit -1
 fi
 # 进入上层目录
 cd ..
 # 检查docs目录是否存在
 if [ ! -d "docs" ];then
-    echo -e "\033[32mclone...\033[0m"
+    beautify_echo "clone repo..." info
     git clone $docs_remote_url
 else
-    echo -e "\033[32mfetch...\033[0m"
+    beautify_echo "fetch repo..." info
     cd docs
     git fetch origin
 fi
 # 切换到指定分支
-echo -e "\033[32mcheckout $doc_branch...\033[0m"
+beautify_echo "checkout brach to $doc_branch..." info
 git checkout $doc_branch
 git checkout .
 git pull origin $doc_branch
 
 # 拷贝不同客户翻译文件
-echo -e "\033[32mcopy customer i18n files...\033[0m"
+beautify_echo "copy customer i18n files..." info
 for((i=0;i<${#folder[*]};i++)) do
-  echo -e "\033[32mcopy i18n files for customer:" customer[i] "...\033[0m"
+  beautify_echo "copy i18n files for customer: ${customer[i]}..." info
   cd mercku/trans/${folder[i]}
   for((i=0;i<${#source[*]};i++)) do
     cp -f ${source[i]} $project_dir/src/i18n/${customer[i]}/${target[i]}
   done
+  beautify_echo "complete copy i18n files for customer: ${customer[i]}..." success
   cd ..
 done
-echo -e "\033[32mcomplete...\033[0m"
+beautify_echo "complete copy all customers i18n files..." success
 
 # 拷贝错误码文件，错误码文件是共用的，拷贝mercku的错误码文件即可
-echo -e "\033[32mcopy error files...\033[0m"
+beautify_echo "complete copy error files..." info
 cp -f app_code/$sourceerror $project_dir/src/i18n/$targeterror
-echo -e "\033[32mcomplete...\033[0m"
+beautify_echo "complete copy error files..." success
 
 
 # 拷贝时区文件
-echo -e "\033[32mcopy timezone files...\033[0m"
+beautify_echo "copy timezones files..." info
 cp -r timezone/* $project_dir/src/timezones
-echo -e "\033[32mcomplete...\033[0m"
+beautify_echo "complete copy timezones files..." success
