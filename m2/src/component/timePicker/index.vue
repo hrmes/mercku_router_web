ac9c4f60812c6750b025c0ff6fa4b8d38ad89ce8
Tingpei Yang <tingpei.yang@hyku.com>
2020-11-27 17:34:13 +0800

    Modify time-picker min-height

--- m2/src/component/timePicker/index.vue
+++ m2/src/component/timePicker/index.vue
@@ -263,7 +263,8 @@ export default {
     display: flex;
     align-items: center;
     width: 100%;
-    height: 36px;
+    height: 100%;
+    min-height: 36px;
     cursor: pointer;
     input {
       cursor: pointer;
