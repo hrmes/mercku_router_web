9564dba5b6ddc16b37308f059153d46eaa471717
ytppp <651003708@qq.com>
2020-12-23 18:27:56 +0800

    Modify issue

--- m2/src/pages/bussiness/setting/ipv6.vue
+++ m2/src/pages/bussiness/setting/ipv6.vue
@@ -581,6 +581,7 @@ export default {
         font-weight: bold;
         text-align: left;
         width: 150px;
+        flex-shrink: 0;
       }
       span {
         display: inline-block;
@@ -597,7 +598,6 @@ export default {
     .form-warn {
       font-size: 16px;
       font-weight: 500;
-      text-align: justify;
       color: #d6001c;
       margin-bottom: 30px;
     }
