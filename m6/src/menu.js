ee2ab0f9f41587603fd4b8b933208abcd52d84f7
ytppp <651003708@qq.com>
2020-12-23 17:23:16 +0800

    Modify menu config

--- m6/src/menu.js
+++ m6/src/menu.js
@@ -7,6 +7,11 @@ export default function getMenu(role, mode = RouterMode.router) {
   console.log(`mode is: ${mode}`);
 
   const allCustomers = Object.keys(Customers).map(key => Customers[key]);
+  // 默认客户配置
+  const defaultCustomerConfig = {
+    auth: [Role.admin],
+    mode: [RouterMode.router, RouterMode.bridge]
+  };
   const wifi = {
     icon: 'wifi',
     text: 'trans0173',
@@ -15,25 +20,31 @@ export default function getMenu(role, mode = RouterMode.router) {
         text: 'trans0365',
         name: 'mesh',
         url: '/dashboard/mesh/topo',
-        super: false,
-        mode: [RouterMode.router, RouterMode.bridge],
-        customers: allCustomers
+        customersConfig: defaultCustomerConfig
+        // super: false,
+        // mode: [RouterMode.router, RouterMode.bridge],
+        // customers: allCustomers
       },
       {
         text: 'trans0235',
         name: 'device',
         url: '/dashboard/device/primary',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         text: 'trans0366',
         name: 'internet',
         url: '/dashboard/internet',
-        super: false,
-        mode: [RouterMode.router, RouterMode.bridge],
-        customers: allCustomers
+        customersConfig: defaultCustomerConfig
+        // super: false,
+        // mode: [RouterMode.router, RouterMode.bridge],
+        // customers: allCustomers
       }
     ]
   };
@@ -45,80 +56,110 @@ export default function getMenu(role, mode = RouterMode.router) {
         text: 'trans0103',
         name: 'wifi',
         url: '/setting/wifi',
-        super: false,
-        mode: [RouterMode.router, RouterMode.bridge],
-        customers: allCustomers
+        customersConfig: defaultCustomerConfig
+        // super: false,
+        // mode: [RouterMode.router, RouterMode.bridge],
+        // customers: allCustomers
       },
       {
         text: 'trans0142',
         name: 'wan',
         url: '/setting/wan',
-        super: false, // true为超级管理员才有的功能
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         text: 'trans0561',
         name: 'safe',
         url: '/setting/safe',
-        super: false,
-        mode: [RouterMode.router, RouterMode.bridge],
-        customers: allCustomers
+        customersConfig: defaultCustomerConfig
+        // super: false,
+        // mode: [RouterMode.router, RouterMode.bridge],
+        // customers: allCustomers
       },
       {
         url: '/setting/super',
         name: 'guest',
         text: 'trans0576',
-        super: false,
-        mode: [RouterMode.router, RouterMode.bridge],
-        customers: [
-          Customers.cik,
-          Customers.internal,
-          Customers.startca,
-          Customers.inverto,
-          Customers.orion
-        ]
+        customers: {
+          [Customers.cik]: defaultCustomerConfig,
+          [Customers.internal]: defaultCustomerConfig,
+          [Customers.startca]: defaultCustomerConfig,
+          [Customers.inverto]: defaultCustomerConfig,
+          [Customers.orion]: defaultCustomerConfig
+        }
+        // super: false,
+        // mode: [RouterMode.router, RouterMode.bridge],
+        // customers: [
+        //   Customers.cik,
+        //   Customers.internal,
+        //   Customers.startca,
+        //   Customers.inverto,
+        //   Customers.orion
+        // ]
       },
       {
         text: 'trans0020',
         name: 'blacklist',
         url: '/setting/blacklist',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         text: 'trans0272',
         name: 'timezone',
         url: '/setting/timezone',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         text: 'trans0639',
         name: 'region',
         url: '/setting/region',
-        super: false,
-        mode: [RouterMode.router, RouterMode.bridge],
-        customers: allCustomers
+        customersConfig: defaultCustomerConfig
+        // super: false,
+        // mode: [RouterMode.router, RouterMode.bridge],
+        // customers: allCustomers
       },
       {
         url: '/setting/guest',
         name: 'guest',
         text: 'trans0538',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
 
       {
         url: '/setting/upnp',
         name: 'upnp',
         text: 'trans0644',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       }
     ]
   };
@@ -130,111 +171,184 @@ export default function getMenu(role, mode = RouterMode.router) {
         url: '/advance/portforwarding',
         name: 'advance-portforwarding',
         text: 'trans0422',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/dmz',
         name: 'advance-dmz',
         text: 'trans0420',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/dhcp',
         name: 'advance-dhcp',
         text: 'trans0417',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/rsvdip',
         name: 'advance-rsvdip',
         text: 'trans0444',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/mac',
         name: 'advance-mac',
         text: 'trans0474',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/ddns',
         name: 'advance-ddns',
         text: 'trans0418',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/vpn',
         name: 'advance-vpn',
         text: 'trans0402',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/mode',
         name: 'advance-mode',
         text: 'trans0539',
-        super: false,
-        mode: [RouterMode.router, RouterMode.bridge],
-        customers: [Customers.mercku, Customers.inverto, Customers.orion]
+        customers: {
+          [Customers.mercku]: defaultCustomerConfig,
+          [Customers.inverto]: defaultCustomerConfig,
+          [Customers.orion]: defaultCustomerConfig
+        }
+        // super: false,
+        // mode: [RouterMode.router, RouterMode.bridge],
+        // customers: [Customers.mercku, Customers.inverto, Customers.orion]
       },
       {
         url: '/advance/diagnosis',
         name: 'advance-diagnosis',
         text: 'trans0419',
-        super: false,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/log',
         name: 'advance-log',
         text: 'trans0421',
-        super: true,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: true,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/firewall',
         name: 'advance-firewall',
         text: 'trans0424',
-        super: true,
-        mode: [RouterMode.router],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        }
+        // super: true,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/wwa',
         name: 'advance.wwa',
         text: 'trans0511',
-        mode: [RouterMode.router],
-        super: [Customers.realnett],
-        customers: allCustomers
+        customersConfig: {
+          auth: [Role.admin],
+          mode: [RouterMode.router]
+        },
+        customers: {
+          [Customers.realnett]: {
+            auth: [Role.super],
+            mode: [RouterMode.router]
+          }
+        }
+        // super: false,
+        // mode: [RouterMode.router],
+        // customers: allCustomers
       },
       {
         url: '/advance/remote/tr069',
         name: 'advance-remote',
         text: 'trans0286',
-        mode: [RouterMode.router],
-        super: true,
-        customers: [
-          Customers.cik,
-          Customers.internal,
-          Customers.startca,
-          Customers.inverto,
-          Customers.orion
-        ]
+        customers: {
+          [Customers.cik]: {
+            auth: [Role.admin],
+            mode: [RouterMode.router]
+          },
+          [Customers.internal]: {
+            auth: [Role.admin],
+            mode: [RouterMode.router]
+          },
+          [Customers.startca]: {
+            auth: [Role.admin],
+            mode: [RouterMode.router]
+          },
+          [Customers.orion]: {
+            auth: [Role.admin],
+            mode: [RouterMode.router]
+          }
+        }
+        // mode: [RouterMode.router],
+        // super: true,
+        // customers: [
+        //   Customers.cik,
+        //   Customers.internal,
+        //   Customers.startca,
+        //   Customers.inverto,
+        //   Customers.orion
+        // ]
       }
     ]
   };
@@ -246,64 +360,95 @@ export default function getMenu(role, mode = RouterMode.router) {
         url: '/upgrade/online',
         name: 'online',
         text: 'trans0202',
-        mode: [RouterMode.router, RouterMode.bridge],
-        customers: allCustomers
+        customersConfig: defaultCustomerConfig
+        // mode: [RouterMode.router, RouterMode.bridge],
+        // customers: allCustomers
       },
       {
         url: '/upgrade/offline',
         name: 'offline',
         text: 'trans0204',
-        mode: [RouterMode.router, RouterMode.bridge],
-        customers: allCustomers
+        customersConfig: defaultCustomerConfig
+        // mode: [RouterMode.router, RouterMode.bridge],
+        // customers: allCustomers
       },
       {
         url: '/upgrade/auto',
         name: 'auto',
         text: 'trans0743',
-        mode: [RouterMode.router, RouterMode.bridge],
-        customers: allCustomers
+        customersConfig: defaultCustomerConfig
+        // mode: [RouterMode.router, RouterMode.bridge],
+        // customers: allCustomers
       }
     ]
   };
   [wifi, setting, advance, upgrade].forEach(item => {
+    // 构造菜单项的客户列表
+    item.children.map(menu => {
+      // customersConfig：指“默认配置”，每个菜单项只需要配置一次，所有客户基于它生成当前菜单项自己的配置。
+      // customers：指“客户自定义配置”，可以配置多个
+      // customersConfig与customers必须配置一个
+      // 存在customersConfig但不存在customers，所有客户基于customersConfig生成自己的配置
+      // 存在customers但不存在customersConfig，使用customers
+      // customersConfig与customers都存在，用customers部分替换基于customersConfig生成自己的配置
+
+      if (menu.customersConfig && !menu.customers) {
+        menu.customers = {};
+        const { customersConfig } = menu;
+        allCustomers.forEach(val => {
+          menu.customers[val] = customersConfig;
+        });
+        delete menu.customersConfig;
+      } else if (menu.customersConfig && menu.customers) {
+        const customers = Object.assign({}, menu.customers);
+        const customersKeyList = Object.keys(customers);
+        const { customersConfig } = menu;
+        allCustomers.forEach(val => {
+          customersKeyList.forEach(cVal => {
+            if (cVal === val) {
+              menu.customers[val] = customers[cVal];
+            } else {
+              menu.customers[val] = customersConfig;
+            }
+          });
+        });
+        delete menu.customersConfig;
+      }
+    });
+
     // 根据id选择对应的菜单项
-    const filter = c => c.customers.includes(process.env.CUSTOMER_CONFIG.id);
+    const filter = menu => {
+      const { customers } = menu;
+      const menuCustomers = Object.keys(customers);
+      const nowCustomer = menuCustomers.find(
+        val => val === process.env.CUSTOMER_CONFIG.id
+      );
+      menu.customers = {
+        [nowCustomer]: customers[nowCustomer]
+      };
+      return menuCustomers.includes(process.env.CUSTOMER_CONFIG.id);
+    };
     item.children = item.children.filter(filter);
     // 如果支持多级管理员，根据角色过滤选择对应的菜单项
     if (process.env.CUSTOMER_CONFIG.allow2LevelAdmin && role !== Role.super) {
-      const filterAuth = a => {
-        let flag = false;
-        // 不存在super时，当做false处理
-        if (!a.super) {
-          a.super = false;
-        }
-        // 如果super是boolean类型，处理与之前类似
-        if (typeof a.super === 'boolean') {
-          flag = !a.super;
-        }
-        // 如果super是array类型，为空数组时，当作false处理;
-        // 当为某些客户组成的数组时，这些客户当作false处理。
-        if (a.super instanceof Array) {
-          if (a.super.length) {
-            if (a.super.includes(process.env.CUSTOMER_CONFIG.id)) {
-              flag = false;
-            }
-          } else {
-            flag = !a.super.length;
-          }
-        }
-        return flag;
+      const filterAuth = menu => {
+        const { customers } = menu;
+        console.log('customers', customers);
+        return customers[process.env.CUSTOMER_CONFIG.id].auth[0] === Role.admin;
       };
       item.children = item.children.filter(filterAuth);
     }
-    console.log(item.children);
 
     // 根据模式选择对应的菜单项
-    item.children.forEach(c => {
-      c.disabled = false;
-      if (!c.mode.includes(mode)) {
-        c.disabled = true;
-      }
+    item.children.forEach(child => {
+      child.disabled = false;
+      const { customers } = child;
+      const menuCustomers = Object.keys(customers);
+      menuCustomers.forEach(val => {
+        if (!customers[val].mode.includes(mode)) {
+          child.disabled = true;
+        }
+      });
     });
   });
 
