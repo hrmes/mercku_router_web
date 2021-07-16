9d3f3169a36afe6519a42c5184008bd9f3129fe2
ytppp <651003708@qq.com>
2021-07-16 17:30:50 +0800

    fix: update timezone0ffset method

--- base/src/mixins/timezone-offset.js
+++ base/src/mixins/timezone-offset.js
@@ -1,13 +1,30 @@
+const addZeroFront = val => (val < 10 ? `0${val}` : val);
 export default {
+  data() {
+    return {
+      timezones: []
+    };
+  },
+  created() {
+    this.timezones = require(`../timezones/${this.$i18n.locale}.json`);
+  },
   methods: {
     isSameTimezoneOffset() {
-      const timezoneOffset = new Date().getTimezoneOffset() / 60;
+      const timezoneOffset = new Date().getTimezoneOffset(); // 获取本地时间与GMT的分钟差。
+      const timezoneOffsetAbs = Math.abs(timezoneOffset);
+      const symbol = timezoneOffset < 0 ? '-' : '';
+      const hour = addZeroFront(timezoneOffsetAbs / 60);
+      const minute = addZeroFront(timezoneOffsetAbs % 60);
+      const timezoneDetailNow = `GMT${symbol}${hour}:${minute}`;
       this.$http.getTimezone().then(res => {
-        const { offset } = res.data.result;
-        if (timezoneOffset !== offset) {
+        const { offset, position } = res.data.result;
+        const { timezoneDetail } = this.timezones.find(
+          item => item.offset === offset && item.position === position
+        );
+        if (timezoneDetailNow !== timezoneDetail) {
           this.$dialog.confirm({
             okText: this.$t('trans0024'),
-            cancelText: this.$t('trans0925'),
+            cancelText: this.$t('trans0926'),
             message: this.$t('trans0925'),
             callback: {
               ok: () => {
