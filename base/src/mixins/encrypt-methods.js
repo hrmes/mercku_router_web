d5e4358cca03e93a6d3c24cf11a8b5c3d1f1dab6
ytppp <651003708@qq.com>
2021-12-06 17:19:07 +0800

    update encrypt

--- base/src/mixins/encrypt-methods.js
+++ base/src/mixins/encrypt-methods.js
@@ -21,29 +21,6 @@ export default {
         ];
         break;
       case Models.m6:
-        encryptMethods = [
-          {
-            value: EncryptMethod.wpa2wpa3,
-            text: this.$t('trans0573')
-          },
-          {
-            value: EncryptMethod.wpawpa2,
-            text: this.$t('trans0557')
-          },
-          {
-            value: EncryptMethod.wpa3,
-            text: this.$t('trans0572')
-          },
-          {
-            value: EncryptMethod.wpa2,
-            text: this.$t('trans0556')
-          },
-          {
-            value: EncryptMethod.open,
-            text: this.$t('trans0554')
-          }
-        ];
-        break;
       case Models.m6c:
         encryptMethods = [
           {
