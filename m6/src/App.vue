143b27f1fae36f9eb15b2473ac533c2ceb1b9588
ytppp <651003708@qq.com>
2021-12-28 16:30:20 +0800

    update header nav color

--- m6/src/App.vue
+++ m6/src/App.vue
@@ -6,7 +6,7 @@
              class="flex-wrap"
              :class="{ 'has-menu': navVisible }">
           <m-header :navVisible="navVisible"
-                    :isLoginPage="isLoginPage"
+                    :isLoginPage="!logoVisible"
                     :logoVisible="logoVisible"
                     :navs="menus"
                     class="header"></m-header>
