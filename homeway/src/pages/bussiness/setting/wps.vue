29bf9b533d09c8bd67a219fc8b263fabcd93f69e
AtroxZhong <zhiheng.zhong@hyku.com>
2024-01-11 18:38:03 +0800

    Revert "feat(frontend): update wps.vue"
    This reverts commit ac3d4f42f2eefb1899e2c5aa59ad5b29c8cb0cb7.

--- homeway/src/pages/bussiness/setting/wps.vue
+++ homeway/src/pages/bussiness/setting/wps.vue
@@ -15,9 +15,8 @@
         </m-form-item>
         <m-form-item>
           <button class="btn"
-                  :disabled="wpsIsEnabled"
                   v-defaultbutton
-                  @click="submit()">{{wpsIsEnabled?'WPS正在启用中...':$t('trans0462')}}</button>
+                  @click="submit()">{{$t('trans0462')}}</button>
         </m-form-item>
       </m-form>
     </div>
@@ -30,7 +29,6 @@ export default {
   data() {
     return {
       wpsBand: '2g',
-      wpsIsEnabled: false,
       bandList: [
         { value: '2g', text: this.$t('trans0677') },
         { value: '5g', text: this.$t('trans0679') }
@@ -40,67 +38,35 @@ export default {
   mounted() {
     this.getWps();
   },
-  beforeDestroy() {
-    this.clearIntervalTask();
-  },
   methods: {
-    async getWps(isFirstEnterPage = true) {
+    async getWps() {
       try {
-        // Show loading when user is first entering into page
-        if (isFirstEnterPage) {
-          this.$loading.open();
-        }
-
+        this.$loading.open();
         const response = await this.$http.getMeshWps();
-        let {
+        const {
           data: {
             result: { bands }
           }
         } = response;
-        bands = [
-          {
-            band: '2g',
-            wps_enabled: false
-          },
-          {
-            band: '5g',
-            wps_enabled: false
-          }
-        ];
-        this.wpsIsEnabled = bands.some(item => {
+        let showFirst = false;
+        bands.forEach(item => {
           if (item.wps_enabled) {
             this.wpsBand = item.band;
+            if (item.band === '2g') showFirst = true;
           }
-          return item.wps_enabled;
         });
-
-        // WPS每开启一次,有效时长为2分钟, WPS启用期间需要轮询getWps接口,
-        // 目的为了界面能及时更新WPS2分钟超时之后的关闭的状态，在这之后WPS才能够再次触发
-        if (this.wpsIsEnabled) {
-          this.clearIntervalTask();
-          this.pollTimer = setTimeout(() => {
-            this.getWps(false);
-          }, 5000);
-        } else { // WPS是关闭的状态下，不再需要轮询getWps
-          this.clearIntervalTask();
-        }
-      } catch {
-        this.clearIntervalTask();
-        this.wpsIsEnabled = false;
+        if (showFirst) this.wpsBand = '2g';
+      } catch (error) {
+        console.error(error);
       } finally {
         this.$loading.close();
       }
-    },
-    clearIntervalTask() {
-      clearTimeout(this.pollTimer);
-      this.pollTimer = null;
     },
     submit: debounce(function updateWps() {
       this.$loading.open();
       this.$http
         .updateMeshWps({ band: this.wpsBand, wps_enabled: true })
         .then(() => {
-          this.getWps();
           this.$toast(this.$t('trans0040'), 3000, 'success');
         })
         .finally(() => {
