2f25ea5a635ebe9c156a32090575156ef7de0656
Yalong Zou <yalong.zou@mercku.com>
2024-08-31 10:59:43 +0200

    support hidden power-supply

--- m6a/src/menu.js
+++ m6a/src/menu.js
@@ -164,7 +164,7 @@ export default function getMenu(role, mode = RouterMode.router, settings = {}) {
       },
       {
         url: '/setting/powersupply',
-        name: 'powersupply',
+        name: 'power-supply',
         text: 'trans1239',
         config
       },
